58c2437fcef7f6c67ebb4da4da69f06f60cc5b84
Martynas Bardauskas <martynasb@wix.com>
2019-07-31 16:56:06 +0300

    app changes to show an activity

--- App.js
+++ App.js
@@ -6,7 +6,7 @@
  * @flow
  */
 
-import React, {Fragment} from 'react';
+import React, {Fragment, useState} from 'react';
 import {
   SafeAreaView,
   StyleSheet,
@@ -14,25 +14,36 @@ import {
   View,
   Text,
   StatusBar,
+  TouchableOpacity,
 } from 'react-native';
 
 const App = () => {
+  const [isActivityShown, setIsActivityShown] = useState(false);
   return (
     <View testID="mainScreen">
       <Fragment>
         <StatusBar barStyle="dark-content" />
         <SafeAreaView>
-          <ScrollView
-            contentInsetAdjustmentBehavior="automatic"
-          >
-          <View>
-            <Text>Hello world</Text>
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
+          <ScrollView contentInsetAdjustmentBehavior="automatic">
+            <View>
+              <TouchableOpacity onPress={() => setIsActivityShown(true)} testID="GET_ACTIVITY">
+                <View><Text>I'm bored</Text></View>
+              </TouchableOpacity>
+              <Activity isActivityShown={isActivityShown}/>
+            </View>
+          </ScrollView>
+        </SafeAreaView>
+      </Fragment>
     </View>
   );
+};
+
+const Activity = ({isActivityShown}) => {
+  if (isActivityShown) {
+    return (<View><Text testID="ACTIVITY_TEXT">learn to play banjo</Text></View>);
+  }
+
+  return null;
 };
 
 const styles = StyleSheet.create({
--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,30 @@
-/**
- * @format
- */
-
 import 'react-native';
 import React from 'react';
 import App from '../App';
+import {componentDriver, getTextNodes} from 'react-component-driver';
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
+describe('Bored app', () => {
+  it('renders button and no activity by default', () => {
+    const component = driver();
+    expect(component.getActivity()).toBeUndefined();
+  });
 
-it('renders correctly', () => {
-  renderer.create(<App />);
+  it('renders renders activity when pressing activity button', () => {
+    const component = driver();
+
+    component.pressActivityButton();
+    expect(component.getActivity()).toEqual('learn to play banjo');
+  });
+});
+
+
+const driver = () => componentDriver(App, {
+  pressActivityButton() {
+    const button = this.getByID('GET_ACTIVITY');
+    button.props.onClick();
+    return this;
+  },
+  getActivity() {
+    return getTextNodes(this.getByID('ACTIVITY_TEXT'))[0];
+  },
 });
